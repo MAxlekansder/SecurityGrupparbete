6e6cf9561a89d60ad168fb93dd1bf500ece6c22d
MAxlekansder <alexander.hasslund@stud.sti.se>
2024-06-08 01:32:09 +0200

    first commit

---
+++ src/main/java/org/example/securitygrupparbete/SecurityGrupparbeteApplication.java
@@ -0,0 +1,13 @@
+package org.example.securitygrupparbete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityGrupparbeteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityGrupparbeteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/securitygrupparbete/SecurityGrupparbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.securitygrupparbete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityGrupparbeteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
