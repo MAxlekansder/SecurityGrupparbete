423cf31643df6aff571776c2baae910f5c1b4096
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-06-12 09:41:03 +0200

    pre pull

--- src/test/java/org/example/securitygrupparbete/Controller/UserControllerTest.java
+++ src/test/java/org/example/securitygrupparbete/Controller/UserControllerTest.java
@@ -1,21 +1,58 @@
 package org.example.securitygrupparbete.Controller;
 
 import org.example.securitygrupparbete.Configuration.SecurityConfiguration;
+import org.example.securitygrupparbete.Repository.UserRepository;
+import org.example.securitygrupparbete.Service.UserDetailsServiceImpl;
 import org.example.securitygrupparbete.Service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest({UserService.class})
+@WebMvcTest({UserController.class})
 @Import({SecurityConfiguration.class})
 class UserControllerTest {
 
     @Autowired
-    MockMvc mvc;
+    private MockMvc mvc;
+
+    @Mock
+    private UserDetailsServiceImpl userDetailsService;
+    @Mock
+    private UserRepository userRepository;
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void homePage() {
+    }
+
+    @Test //Oskar
+    @WithMockUser(username = "Admin", password ="1234")
+    void loginUserSuccessfully() throws Exception {
+
+    }
+
+    @Test //Oskar
+
+    void loginUserUnsuccessfully() throws Exception {
+        mvc.perform(get("/")).andExpect(status().isOk());
+
+
+    }
 }
